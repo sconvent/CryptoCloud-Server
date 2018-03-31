ca43e6ad3ca81aa5e5287e7f8259673166ec5014
simon <simon@convnet.de>
2018-03-31 11:21:02 +0200

    Remove salt table from model

--- code/cryptoCloudModel.sql
+++ code/cryptoCloudModel.sql
@@ -2,14 +2,7 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
--- -----------------------------------------------------
--- Schema cryptoCloud
--- -----------------------------------------------------
 DROP SCHEMA IF EXISTS `cryptoCloud` ;
-
--- -----------------------------------------------------
--- Schema cryptoCloud
--- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `cryptoCloud` ;
 USE `cryptoCloud` ;
 
@@ -77,18 +70,6 @@ CREATE TABLE IF NOT EXISTS `cryptoCloud`.`access_token` (
     REFERENCES `cryptoCloud`.`user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `cryptoCloud`.`salt`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `cryptoCloud`.`salt` ;
-
-CREATE TABLE IF NOT EXISTS `cryptoCloud`.`salt` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `auth_client_salt` VARCHAR(22) NOT NULL,
-  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
